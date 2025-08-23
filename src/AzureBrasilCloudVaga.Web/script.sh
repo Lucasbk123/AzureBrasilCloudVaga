bfc70734b65718a7372665ce9686f2949cc0495e
Lucas pereira <lucas_trucoo@hotmail.com>
2025-08-23 00:36:23 -0300

    docs: removendo comentarios

--- src/AzureBrasilCloudVaga.Web/script.sh
+++ src/AzureBrasilCloudVaga.Web/script.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 echo "Executando script customizado antes do NGINX iniciar..."
 
-
-# Arquivo JSON
 JSON_FILE="/usr/share/nginx/html/appsettings.json"
 
 echo "Atualiza o JSON usando variáveis de ambiente do container"
 
-# Converte a variável de ambiente NEW_SCOPE (separada por vírgula) em um array JSON
 json_array=$(printf '%s' "$NEW_SCOPE" | tr ',' '\n' | jq -R . | jq -s .)
 
-# Atualiza o JSON substituindo os valores
 jq --arg authority "$AZURE_AUTHORITY" \
    --arg clientId "$CLIENT_ID" \
    --arg baseUrl "$API_BASEURL" \
